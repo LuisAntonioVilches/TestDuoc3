7c483588c9d4ee7cbaefb441de7f4d785dee106a
HP <HP@10.32.168.49>
2019-03-26 12:51:50 -0300

    Cambio 1

---
+++ src/testduoc3/TestDuoc3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testduoc3;
+
+/**
+ *
+ * @author HP
+ */
+public class TestDuoc3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //cambio1
+    }
+    
+}
